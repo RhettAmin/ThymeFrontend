e97fbfd0182afeef6829ec6d46d427799c7b65a3
Rhett Amin <rhettamin@gmail.com>
2026-03-11 20:56:23 -0400

    Update firebaseConfig.ts

--- app/api/firebase/firebaseConfig.ts
+++ app/api/firebase/firebaseConfig.ts
@@ -5,19 +5,19 @@ import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 const firebaseApp = initializeApp({
   // application settings
-  apiKey: "AIzaSyDmpLPfrkRjskDp4P2fSbLvMUrHjEq9DWA",
-  authDomain: "primeval-stack-408400.firebaseapp.com",
-  projectId: "primeval-stack-408400",
-  storageBucket: "primeval-stack-408400.appspot.com",
-  messagingSenderId: "462093202517",
-  appId: "1:462093202517:web:17d506ecb43e7e59a7d0dc",
-  measurementId: "G-YEEV13GE43",
+  apiKey: "api_key",
+  authDomain: "<code>.firebaseapp.com",
+  projectId: "<code>",
+  storageBucket: "<code>.appspot.com",
+  messagingSenderId: "<code>",
+  appId: "<code>",
+  measurementId: "<code>",
 })
 
 // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
 // key is the counterpart to the secret key you set in the Firebase console.
 const appCheck = initializeAppCheck(firebaseApp, {
-  provider: new ReCaptchaV3Provider('6LfFnp8qAAAAACvDsqNKT0wQJdH4JcuizCcle6fo'),
+  provider: new ReCaptchaV3Provider('<code>'),
 
   // Optional argument. If true, the SDK automatically refreshes App Check
   // tokens as needed.
